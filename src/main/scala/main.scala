b8e58e7c7a225bd7ac49931c7e46e096b46b6f12
Kresten Laust <krestenlaust@gmail.com>
2024-04-22 23:08:10 +0200

    Add Main to test

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,20 @@
+import org.antlr.v4.runtime._
+import org.antlr.v4.runtime.tree.ParseTreeWalker
+
 @main
-def main(): Unit = {
+def main(): Unit =
   println("Hello world!")
-}
+  val input = "3 + 4 * 5 - 2"
+
+  val lexer = InfixMathLexer(CharStreams.fromString(input))
+  val tokens = CommonTokenStream(lexer)
+  val parser = InfixMathParser(tokens)
+
+  val listener = InfixMathListenerImpl()
+  val tree = parser.mainExpr()
+
+  val walker = ParseTreeWalker()
+
+  walker.walk(listener, tree)
+
+  println(listener.result)
